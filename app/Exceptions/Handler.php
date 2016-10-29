83eef93da38820015a8d77126b9901cb4b376f02
Narek Vardanjan <vnarek@gmail.com>
2016-10-29 16:21:33 +0200

    Update exception handler for json outputs

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,11 @@
 namespace KC\Exceptions;
 
 use Exception;
+use Illuminate\Http\Response;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -27,34 +30,63 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param  \Exception  $e
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Exception $e)
     {
-        parent::report($exception);
+        parent::report($e);
     }
 
     /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Exception  $e
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $e)
     {
-        return parent::render($request, $exception);
+        if(! $request->expectsJson()) {
+          return parent::render($request, $e);
+        }
+        return $this->renderJson($e);
     }
-
+    /**
+      * Render an exception into an HTTP response in Json
+      * @param \Exception $e
+      * @return \Illuminate\Http\Response
+      */
+    private function renderJson(Exception $e)
+    {
+        if($e instanceof ModelNotFoundException) {
+          return $this->responseJson($e->getMessage(), 404);
+        }
+        if($e instanceof HttpException) {
+          return $this->responseJson($e->getMessage(), $e->getStatusCode());
+        }
+        return $this->responseJson($e->getMessage(), 500);
+    }
+    /**
+     * Format a json response
+     * @param string $message
+     * @param int    $code
+     * @return \Illuminate\Http\Response
+     */
+    private function responseJson($message, $code = 500) {
+        return response()->json([
+          'success' => false,
+          'message' => $message
+        ], $code);
+    }
     /**
      * Convert an authentication exception into an unauthenticated response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @param  \Illuminate\Auth\AuthenticationException  $e
      * @return \Illuminate\Http\Response
      */
-    protected function unauthenticated($request, AuthenticationException $exception)
+    protected function unauthenticated($request, AuthenticationException $e)
     {
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
