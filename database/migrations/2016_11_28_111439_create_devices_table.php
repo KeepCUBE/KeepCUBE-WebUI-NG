0dce99a97e73e0d5222e4a311d68cdc83b041ad6
Narek Vardanjan <vnarek@gmail.com>
2016-12-03 17:12:42 +0100

    Add database migrations for devices

---
+++ database/migrations/2016_11_28_111439_create_devices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('type_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('devices');
+    }
+}
