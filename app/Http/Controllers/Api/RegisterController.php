140a3d1e0d1b38cef755381a02439b9e48afe9dd
Narek Vardanjan <vnarek@gmail.com>
2017-03-21 23:33:49 +0100

    Add register method for register via api

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -38,11 +38,10 @@ class RegisterController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function register(Request $request, Response $response)
+    public function register(Request $request)
     {
-        $data = $request->only(['email', 'password','name']);
-
-        $validation = $this->validator($request->all());
+        $data = $request->only(['email', 'password','username', 'password_confirmation']);
+        $validation = $this->validator($data);
 
         if($validation->fails()) {
           return $validation->errors();
