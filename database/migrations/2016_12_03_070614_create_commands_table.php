95c5f3f1f4a5e39c8434be315d407a767e735f28
Narek Vardanjan <vnarek@gmail.com>
2017-04-03 21:54:25 +0200

    Redefine command table migration

--- database/migrations/2016_12_03_070614_create_commands_table.php
+++ database/migrations/2016_12_03_070614_create_commands_table.php
@@ -15,8 +15,10 @@ class CreateCommandsTable extends Migration
     {
         Schema::create('commands', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('command');
+            //$table->string('command');
             $table->integer('type_id');
+            $table->boolean('template');
+            $table->json('scheme');
             $table->timestamps();
         });
     }
