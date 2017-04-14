82bf849f3e746119f04afd063d02561ba59b6af8
Narek Vardanjan <vnarek@gmail.com>
2017-04-14 21:35:35 +0200

    Add factories to new models

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -3,6 +3,7 @@ use KC\Models\User\User;
 use KC\Models\Device\Device;
 use KC\Models\Type\Type;
 use KC\Models\Command\Command;
+use KC\Models\Route\Route;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -24,12 +25,19 @@ $factory->define(User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
-$factory->define(Type::class, function (Faker\Generator $faker) {
+$factory->define(Route::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name
+        'code' => strtoupper($faker->word),
+        'name' => $faker->name,
+        'parent_id' => null
     ];
 });
-
+$factory->define(Type::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name, 
+        'route_id' => factory(Route::class)->create()->id
+    ];
+});
 $factory->define(Device::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
@@ -39,7 +47,24 @@ $factory->define(Device::class, function (Faker\Generator $faker) {
 
 $factory->define(Command::class, function (Faker\Generator $faker) {
     return [
-        'command' => '#SLPL4T2D1Pff0022ff0011ff0022;',
-        'type_id' => factory(Type::class)->create()->id
+        'template' => false,
+        'type_id' => factory(Type::class)->create()->id,
+        'command_scheme' => [
+                'name' => 'SLP',
+                'values' => [
+                    'L' => 25,
+                    'P' => [
+                        '#ff0022',
+                        '#ff0033'
+                    ],
+                    'T' => 0.4,
+                    'D' => 10
+                ]
+        ]
+    ];
+});
+$factory->state(Command::class, 'template', function(Faker\Generator $faker) {
+    return [
+        'template' => true
     ];
 });
