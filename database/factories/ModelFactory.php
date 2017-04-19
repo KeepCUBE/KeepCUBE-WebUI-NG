386dd664713f656b9dd77beaa989f209782f5c76
Narek Vardanjan <vnarek@gmail.com>
2017-04-19 22:10:37 +0200

    Solve merge conflicts ModelFactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,3 +21,52 @@ $factory->define(KC\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(Route::class, function (Faker\Generator $faker) {
+    return [
+        'code' => strtoupper($faker->word),
+        'name' => $faker->name,
+        'parent_id' => null
+    ];
+});
+$factory->define(Type::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name, 
+        'route_id' => factory(Route::class)->create()->id
+    ];
+});
+$factory->define(Room::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->text
+    ];
+});
+$factory->define(Device::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'type_id' => factory(Type::class)->create()->id,
+        'room_id' => factory(Room::class)->create()->id
+    ];
+});
+
+$factory->define(Command::class, function (Faker\Generator $faker) {
+    return [
+        'template' => false,
+        'type_id' => factory(Type::class)->create()->id,
+        'command_scheme' => [
+                'name' => 'SLP',
+                'values' => [
+                    'L' => 1,
+                    'C' => [
+                        '#ff0022'
+                    ],
+                    'T' => 1000
+                ]
+        ]
+    ];
+});
+$factory->state(Command::class, 'template', function(Faker\Generator $faker) {
+    return [
+        'template' => true
+    ];
+});
