88fd500f9a91b5ee278768389c58f2f12a5a8d60
Narek Vardanjan <vnarek@gmail.com>
2017-04-19 17:13:06 +0200

    Modify device table migration

--- database/migrations/2016_11_28_111439_create_devices_table.php
+++ database/migrations/2016_11_28_111439_create_devices_table.php
@@ -17,6 +17,7 @@ class CreateDevicesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('type_id');
+            $table->integer('room_id');
             $table->timestamps();
         });
     }
