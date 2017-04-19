65412d93ea8ac464f67688f81b904db446760d20
Narek Vardanjan <vnarek@gmail.com>
2017-04-19 17:11:16 +0200

    Remove unused exampleScheme

--- tests/CommandExecuteTest.php
+++ tests/CommandExecuteTest.php
@@ -12,25 +12,11 @@ class CommandExecuteTest extends TestCase
     private $headers = [
       'Accept' => 'application/json'
     ];
-
-    private $exampleScheme = [
-        'command_scheme' => [
-                '#' => 'SLP',
-                'L' => 25,
-                'P' => [
-                    '#ff0022',
-                    '#ff0033'
-                ],
-                'T' => 0.4,
-                'D' => 10
-        ]
-    ];
     public function testCommandExecute() {
         $command = factory(Command::class)->create();
         $parent = $command->type->route;
         $parent->children()->create(['name' => 'NRF', 'code' => 'NRF']);
         $response = $this->post(route('commands.execute', [$command->id]));
-        dump($response);
         $response->seeStatusCode(200);
     }
 }
