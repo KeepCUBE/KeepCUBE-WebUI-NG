c41f04adb4a5b41d3a69910be38a284d294b23ab
Dominik Dosoudil <eziotegg@gmail.com>
2017-04-21 14:27:44 +0200

    Add type_id so it gets out of api

--- app/Transformers/DeviceTransformer.php
+++ app/Transformers/DeviceTransformer.php
@@ -7,8 +7,9 @@ use KC\Models\Device\Device;
 class DeviceTransformer extends TransformerAbstract {
     public function transform(Device $device) {
         return [
-          'id' => (int) $device->id,
-          'name' => $device->name,
+            'id' => (int) $device->id,
+            'name' => $device->name,
+            'type_id' => $device->type_id,
         ];
     }
 }
