8ed4d5ede6083c4b149e346177a470ee1ca70de7
Narek Vardanjan <vnarek@gmail.com>
2017-04-22 13:03:18 +0200

    Cleanup api.php redundant routes

--- routes/api.php
+++ routes/api.php
@@ -12,24 +12,5 @@ use Illuminate\Support\Facades\Redis;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-/*Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');*/
-
 Route::post('/auth', 'AuthController@authenticate');
 Route::post('/register', 'RegisterController@register');
-
-
-Route::group([
-  'middleware' => 'jwt',
-  'namespace' => 'V1',
-  'prefix' => 'v1'
-], function() {
-  Route::resource('/users', 'UserController', ['except' => ['create', 'edit']]);
-  Route::resource('/devices', 'DeviceController');
-});
-
-/*Route::get('/action', function () {
-    Redis::publish('keepi:action', json_encode(['#ahojdaw' => '#ajdiwd']));
-  });*/
