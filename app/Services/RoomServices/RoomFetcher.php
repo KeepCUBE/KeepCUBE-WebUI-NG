e3e18a2fd77d3e48116d491165705309cbd278af
Narek Vardanjan <vnarek@gmail.com>
2017-04-23 00:13:19 +0200

    Add possibility to include devices to room fetch

--- app/Services/RoomServices/RoomFetcher.php
+++ app/Services/RoomServices/RoomFetcher.php
@@ -10,26 +10,35 @@ use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 
 class RoomFetcher {
     private $room;
+    /**
+     * @var \Spatie\Fractal\Fractal
+     */
     private $fractal;
 
     public function __construct(Room $room, Fractal $fractal) {
         $this->room = $room;
         $this->fractal = $fractal;
     }
-    public function find($id) {
+    public function find($id, string $includes = null) {
         try {
+            $this->parseIncludes($includes);
             $room = $this->room->findOrFail($id);
             return $this->fractal->item($room, new RoomTransformer);
         } catch (ModelNotFoundException $e) {
             throw new NotFoundHttpException("Device with id {$id} not found");
         }
     }
-    public function paginate(array $query = null, $perPage = 15) {
+    public function paginate(string $includes = null, $perPage = 15) {
         $rooms = $this->room->paginate($perPage);
-
+        $this->parseIncludes($includes);
         return $this->fractal
             ->collection($rooms, new RoomTransformer)
             ->paginateWith(new IlluminatePaginatorAdapter($rooms))
             ->toArray();
+    }
+    public function parseIncludes($includes) {
+        if($includes != null) {
+                $this->fractal->parseIncludes($includes);
+        }
     }
 }
